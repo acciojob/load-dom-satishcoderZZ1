b75fa7a8ee96a9f7e39b22317cf68974be1384f5
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-16 21:16:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
-	document.body.textContent = "Dom load success";
+	document.body.textContent = "DOM load success";
 });
